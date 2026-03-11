2f2b62d95feccd3e7fb7e614de5b383cdfc6e2ca
Yeganehkhazaei <85737524+Yeganehkhazaei@users.noreply.github.com>
2026-03-11 23:57:04 +1100

    Add files via upload
    Added RNA-seq exploratory analysis and expression boxplot by immunophenotype

--- analysis.r
+++ analysis.r
@@ -19,26 +19,49 @@
 # Load libraries -------------------
 # You may use base R or tidyverse for this exercise
 
-# ex. library(tidyverse)
+library(tidyverse)
+library(readr)
 
 # Load data here ----------------------
 # Load each file with a meaningful variable name.
-
+data <- read_csv("data/GSE60450_GeneLevel_Normalized(CPM.and.TMM)_data.csv")
+meta_data <- read_csv("data/GSE60450_filtered_metadata.csv")
 
 
 # Inspect the data -------------------------
 
 # What are the dimensions of each data set? (How many rows/columns in each?)
 # Keep the code here for each file.
+# What are the dimensions of each data set?
+
 
 ## Expression data
-
+dim(data)
+# or nrow(), ncol()
+#answer: [1] 23735 rows    14 columns
 
 ## Metadata
-
+dim(meta_data)
+#answer: [1] 12 rows 4 columns
 
 # Prepare/combine the data for plotting ------------------------
 # How can you combine this data into one data.frame?
+
+# Convert expression matrix to long format
+data_long <- data %>%
+  pivot_longer(
+    cols = starts_with("GSM"),
+    names_to = "sample",
+    values_to = "expression"
+  )
+
+# Merge with metadata
+combined_df <- data_long %>%
+  left_join(meta_data, by = c("sample" = "...1"))
+
+# Check results
+dim(combined_df)
+head(combined_df)
 
 
 
@@ -46,7 +69,16 @@
 ## Plot the expression by cell type
 ## Can use boxplot() or geom_boxplot() in ggplot2
 
+library(ggplot2)
 
-
+ggplot(combined_df, aes(x = immunophenotype, y = expression)) + # we can do y with log too due to skewedness.
+  geom_boxplot() +
+  labs(
+    title = "Gene Expression by Cell Type",
+    x = "Cell Type (Immunophenotype)",
+    y = "Expression"
+  ) +
+  theme_bw()
 ## Save the plot
+ggsave("results/expression_by_celltype.png", width = 8, height = 6)
 ### Show code for saving the plot with ggsave() or a similar function
